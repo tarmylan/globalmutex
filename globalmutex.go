0047ebc054c2250a46a97aa5c2f31760c71f7198
tarmylan <tarmylan@gmail.com>
2018-07-24 14:08:22 +0800

    bug fixed

--- globalmutex.go
+++ globalmutex.go
@@ -20,33 +20,35 @@ type mutex struct {
 type locker struct {
 	inUse sync.Map
 	pool  sync.Pool
+	mu    sync.RWMutex
 }
 
 func (l *locker) Lock(key interface{}) {
-	m := l.get(key)
-	atomic.AddInt64(&m.ref, 1)
+	m := l.get(key, 1)
 	m.mu.Lock()
 }
 
 func (l *locker) RLock(key interface{}) {
-	m := l.get(key)
-	atomic.AddInt64(&m.ref, 1)
+	m := l.get(key, 1)
 	m.mu.RLock()
 }
 
 func (l *locker) Unlock(key interface{}) {
-	m := l.get(key)
+	m := l.get(key, 0)
 	m.mu.Unlock()
 	l.put(key, m)
 }
 
 func (l *locker) RUnlock(key interface{}) {
-	m := l.get(key)
+	m := l.get(key, 0)
 	m.mu.RUnlock()
 	l.put(key, m)
 }
 
 func (l *locker) put(key interface{}, m *mutex) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	atomic.AddInt64(&m.ref, -1)
 	if m.ref <= 0 {
 		l.pool.Put(m.mu)
@@ -54,13 +56,21 @@ func (l *locker) put(key interface{}, m *mutex) {
 	}
 }
 
-func (l *locker) get(key interface{}) *mutex {
+func (l *locker) get(key interface{}, delta int64) *mutex {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
 	res, _ := l.inUse.LoadOrStore(key, &mutex{
 		ref: 0,
 		mu:  l.pool.Get().(*sync.RWMutex),
 	})
 
-	return res.(*mutex)
+	m := res.(*mutex)
+	if delta != 0 {
+		atomic.AddInt64(&m.ref, delta)
+	}
+
+	return m
 }
 
 func NewGlobalMutex() GlobalMutex {
